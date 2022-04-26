1cb73afc266a8507b0f2ff3372999f3392e8a05b
Camelia Rusu <rusu_maria14@yahoo.com>
2022-04-26 09:57:15 +0300

    added new products

---
+++ styles/products.js
@@ -0,0 +1,355 @@
+const produse = [
+    {
+      id: 1,
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0074/0832/0621/products/851770003179-v11-ProteinPowder-2.03lb-Chocolate-Front-HIRES-shopify_800px_540x.jpg?v=1627934769",
+      avatar2:
+        "https://cdn.shopify.com/s/files/1/0074/0832/0621/products/851770003179-v11-ProteinPowder-2.03lb-Chocolate-20201208-NFP-LOI-shopify_800px_540x.jpg?v=1627934769",
+      name: "Organic Protein 500g",
+      price: "25$",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+  
+    {
+      id: 2,
+      avatar:
+        "https://cdn.shopify.com/s/files/1/0283/4009/2988/products/Choc-Powder_540x.png?v=1624909492",
+      avatar2:
+        "https://dl.airtable.com/.attachments/d8f3ac8ba514c957c28ddcccce2ec3f2/52d1b7ed/OO__SLSC_PWD_30_Choc_Website_SFP.jpg",
+      name: "Aloha Protein 500g",
+      price: "30$",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+  
+    {
+      id: 3,
+      avatar:
+        "https://highenergy.ro/983-large_default/vivo-whole-shake-nutritiv-din-plante-aroma-de-caramel-cu-biscuiti-1kg.jpg",
+      avatar2:
+        "https://cdn.shopify.com/s/files/1/0283/4009/2988/products/Choc-Powder_IL-NFP_1024x1024@2x.jpg?v=1624909452",
+      name: "PHD Diet Protein 1kg",
+      price: "40$",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+  
+    {
+      id: 4,
+      avatar:
+        "https://highenergy.ro/1181-large_default/phd-diet-plant-protein-ciocolata-belgiana-500g.jpg",
+      avatar2:
+        "HTTPS://WWW.PHD.COM/MEDIA/CATALOG/PRODUCT/P/H/PHD-DPP_2_.PNG?QUALITY=80&FIT=BOUNDS&HEIGHT=700&WIDTH=700&CANVAS=700:700",
+      name: "Diet Plant Protein 500g",
+      price: "25$",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+      id: 5,
+      avatar:
+        "https://highenergy.ro/409-large_default/vivo-perform-proteine-vegetale-bcaa-aroma-de-caramel-maca-532g.jpg",
+      avatar2:
+        "•	https://highenergy.ro/1182-cart_default/phd-diet-plant-protein-ciocolata-belgiana-500g.jpg",
+      name: "Vivo Protein 500g",
+      price: "30$",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+      id: 6,
+      avatar:
+        "https://highenergy.ro/950-large_default/vivo-whole-shake-nutritiv-din-plante-aroma-de-capsuni-1kg.jpg",
+      avatar2:
+        "•	https://highenergy.ro/1182-cart_default/phd-diet-plant-protein-ciocolata-belgiana-500g.jpg",
+      name: "Vivo Whole Plant 1kg",
+      price: "40$",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    },
+    {
+        id: "7",
+        avatar:
+          "https://static.thcdn.com/images/large/webp//productimg/1600/1600/10529701-1724835571924615.jpg",
+        avatar2:"",
+        name: "MyProtein Soy Protein Isolate 500g",
+        price: "$30"
+      },
+      {
+          id: "8",
+          avatar:
+            "https://highenergy.ro/1450-large_default/vivo-life-vegan-protein-proteine-vegetale-fara-aroma-900g.jpg",
+         avatar2:"",
+          name: "Life Vegan Protein - Flavorless Vegetable Protein 900g",
+          price: "$40"
+        },
+        {
+            id: "9",
+            avatar:
+              "https://m.media-amazon.com/images/I/819LPN1SdzS._AC_SX569_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/81oVRjE-KAS._AC_SX569_.jpg",
+            name: " ALOHA Organic Plant-Based Protein Powder - NO-STEVIA Vanilla 1kg",
+            price: "$40"
+          },
+          {
+            id: "10",
+            avatar:
+              "https://m.media-amazon.com/images/I/81us9nfalBL._AC_SL1500_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/91G6R+USXTL._AC_SL1500_.jpg",
+            name: "Your Super Plant Collagen Powder - Collagen Creamer Alternative, Plant Based, 120g",
+            price: "$40"
+          },
+          {
+            id: "11",
+            avatar:
+              "https://m.media-amazon.com/images/I/71dXQpWpmsL._AC_SX569_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/71OWECgsKlL._AC_SX569_.jpg",
+            name: "PlantVital Plant Based Chocolate Protein Powder, 500g",
+            price: "$30"
+          },
+          {
+            id: "12",
+            avatar:
+              "https://m.media-amazon.com/images/I/61Pct6-WYwL._AC_SY879_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/71wc4XPI6KL._AC_SX569_.jpg",
+            name: "North America Grown, Organic Pea Protein Powder, 1KG ",
+            price: "$45"
+          },
+          {
+            id: "13",
+            avatar:
+              "https://m.media-amazon.com/images/I/41p+Sw0K1zL._AC_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/41p+Sw0K1zL._AC_.jpg",
+            name: "Evogen Greentein -Pure Grain Free Plant Protein, Pea, Pumpkin, Watermelon Seed Proteins, 900g ",
+            price: "$40"
+          },
+          {
+            id: "14",
+            avatar:
+              "https://m.media-amazon.com/images/I/81Mt+JuIT2L._AC_SX569_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/81FF1zUEiaL._AC_SX569_.jpg",
+            name: "Anthony's Premium Pea Protein, Unflavored, 900g ",
+            price: "$40"
+          },
+          {
+            id: "15",
+            avatar:
+              "https://images-na.ssl-images-amazon.com/images/I/6166wkQljJL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/61KXou9gUPL._AC_SX569_.jpg",
+            name: "Misfits Vegan Protein Powder, Salted Caramel, 20g Plant Based Protein, 500g",
+            price: "$30"
+          },
+          {
+            id: "16",
+            avatar:
+              "https://images-na.ssl-images-amazon.com/images/I/71xuhjT9kwL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/71s-OvpYvML._AC_SX569_.jpg",
+            name: " Sunwarrior Classic Vegan Sprouted Brown Rice Protein Powder, 750g",
+            price: "$25"
+          },
+          {
+            id: "17",
+            avatar:
+              "https://images-na.ssl-images-amazon.com/images/I/816w0jZSnXL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/81gTtp8jWLL._AC_SX569_.jpg",
+            name: " Vega Sport Premium Protein Powder, Vanilla, Vegan, 30g Plant Based Protein, 1.8kg",
+            price: "$60"
+          },
+          {
+            id: "18",
+            avatar:
+              "https://images-na.ssl-images-amazon.com/images/I/811b2ym0F6L.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/81bky1T8DYL._AC_SX569_.jpg",
+            name: "Vega Sport Premium Protein Powder, Chocolate, Vegan, 30g Plant Based Protein -1.8kg",
+            price: "$60"
+          },
+          {
+            id: "19",
+            avatar:
+              "https://images-na.ssl-images-amazon.com/images/I/619K0dRbZKL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/71Nvp6OpkXL._AC_SX569_.jpg",
+            name: " Vega Protein and Greens, Vanilla - 609g",
+            price: "$30",
+            description:""
+          },
+          {
+            id: "20",
+            avatar:
+              "https://m.media-amazon.com/images/I/615+iyBB8VL._AC_SX569_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/7195WES2bGL._AC_SX569_.jpg",
+            name: " Vega Protein Powder - Greens and Berry -609g",
+            price: "$30",
+            description:""
+          },  
+          {
+            id: "21",
+            avatar:
+              "https://m.media-amazon.com/images/I/61sppoDW2TL._AC_SX569_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/81Ti0kcKmPL._AC_SX569_.jpg",
+            name: " Garden of Life - Organic Vegan Sport Protein Powder, Vanilla - 800g",
+            price: "$40",
+            description:""
+          },
+          {
+            id: "22",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }, 
+          {
+            id: "23",
+            avatar:
+              "https://m.media-amazon.com/images/I/81NgYh-AKiL._AC_SX569_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/61hJgsFKiNL._AC_SX569_.jpg",
+            name: "Ancient Nutrition Organic Plant Protein - Vanilla, 350g",
+            price: "$30",
+            description:""
+          }, 
+          {
+            id: "24",
+            avatar:
+              "https://m.media-amazon.com/images/I/61x1pony4iL._AC_SY606_.jpg",
+            avatar2:"https://m.media-amazon.com/images/I/71k33gdKA7L._AC_SY606_.jpg",
+            name: "OLLY Plant Powered Protein, 18g Plant Protein -400g ",
+            price: "$35",
+            description:""
+          }, 
+          {
+            id: "25",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }, 
+          {
+            id: "26",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }, 
+          {
+            id: "27",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }, 
+          {
+            id: "28",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }, 
+          {
+            id: "29",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }, 
+          {
+            id: "30",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }, 
+          {
+            id: "31",
+            avatar:
+              "",
+            avatar2:"",
+            name: " ",
+            price: "$",
+            description:""
+          }
+  ];
+  
+  const mainContainer = document.getElementById("produse");
+  const filterInput = document.getElementById("filter_products");
+  const sortingSelection = document.getElementById("sorting");
+  
+  function createProduct(name, avatar, price, id) {
+    const productContainer = document.createElement("article");
+    const imgEl = document.createElement("img");
+    const nameEl = document.createElement("div");
+    const priceEl = document.createElement("div");
+    const linkEl = document.createElement("a");
+    linkEl.textContent = "View details";
+    linkEl.href = `single-product.html?id=${id}`;
+    priceEl.textContent = price;
+    nameEl.textContent = name;
+    imgEl.src = avatar;
+    productContainer.appendChild(imgEl);
+    productContainer.appendChild(nameEl);
+    productContainer.appendChild(priceEl);
+    productContainer.appendChild(linkEl);
+    mainContainer.appendChild(productContainer);
+  }
+  function generateProducts(produseEl) {
+    mainContainer.innerHTML = "";
+    for (const product of produseEl) {
+      createProduct(product.name, product.avatar, product.price, product.id);
+    }
+  }
+  
+  // produse.forEach((produs) => {
+  //   const { name, avatar, price } = produs;
+  //   createProduct(name, avatar, price);
+  // });
+  
+  //alta varianta
+  // for (const product of produse) {
+  //   createProduct(product.name, product.avatar, product.price);
+  // }
+  generateProducts(produse);
+  filterInput.addEventListener("change", (e) => {
+    const filterCriteria = e.target.value;
+    if (filterCriteria !== "") {
+      const filteredProducts = produse.filter((produs) => {
+        const nameCondition = produs.name
+          .toLocaleLowerCase()
+          .includes(filterCriteria.toLocaleLowerCase());
+        const priceCondition = produs.price.includes(filterCriteria);
+        return nameCondition || priceCondition;
+      });
+      generateProducts(filteredProducts);
+      // console.log(filteredProducts);
+    } else {
+      generateProducts(produse);
+    }
+  });
+  
+  sortingSelection.addEventListener("change", (e) => {
+    const sortingValue = e.target.value;
+    let sorted;
+    if (sortingValue === "low") {
+      sorted = produse.sort((a, b) => {
+        return a.price > b.price ? -1 : a.price < b.price ? 1 : 0;
+      });
+    } else {
+      sorted = produse.sort((a, b) => {
+        return a.price < b.price ? -1 : a.price > b.price ? 1 : 0;
+      });
+    }
+    generateProducts(sorted);
+  });
+  
